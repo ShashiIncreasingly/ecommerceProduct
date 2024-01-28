c33e11c5b28e6340d06de23d98fcaf441e0cf824
Shashi <prashant@increasingly.com>
2024-01-28 13:58:38 +0530

    CSS update and contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,15 @@
 const Contact = () => {
-    return <h1>Contact Me</h1>;
+    return (
+      <div className="contact_block">
+          <h1>Contact Me</h1>
+          <div className="contact_wrap">
+          <div className="mob"><span>Mobile No :</span> <span>8892880637</span></div>
+          <div className="email"><span>Email : </span><span>shashi1525@gmail.com</span></div>
+          <div className="linkedin"><span>LinkedIn :</span> <span><a href='https://www.linkedin.com/in/shashbhushan/' target='_blank'>https://www.linkedin.com/in/shashbhushan/</a></span></div>
+          <div className="github"><span>GitHub : </span><span><a href='https://github.com/Shashi1525' target='_blank'>https://github.com/Shashi1525</a></span></div>
+          </div>
+      </div>
+    );
   };
   
   export default Contact;
