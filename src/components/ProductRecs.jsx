df39895ebaacdd6d1ffe49c08d3931238a9cb598
Shashi <prashant@increasingly.com>
2024-02-01 13:10:24 +0530

    check navigation root

--- src/components/ProductRecs.jsx
+++ src/components/ProductRecs.jsx
@@ -12,6 +12,7 @@ function ProductRecs() {
   const { cart, addItemToCart, removeItemFromCart, clearCart } = useCart();
   const [products, setProducts] = useState(null)
   const [activeProducts, setActiveProducts] = useState(null)
+  const [optionChange, setOptionChange] = useState(null)
 
 
   useEffect(() => {
@@ -34,6 +35,19 @@ function ProductRecs() {
   const resetFilter = () => {
     setActiveProducts(products)
   }
+  function sortPrice(val){
+    var productSort;
+    if(val === "default"){
+      productSort = products.sort((a, b) => (a.id - b.id))
+    }else if(val === "decending"){
+      productSort = products.sort((a, b) => ((b.discounted_price ? b.discounted_price : b.price ) - (a.discounted_price ? a.discounted_price : a.price)))
+    }else if(val === "accending"){
+      productSort = products.sort((a, b) => ((a.discounted_price ? a.discounted_price : a.price) - (b.discounted_price ? b.discounted_price : b.price)))
+    }
+    setActiveProducts(productSort)
+    console.log(productSort)
+    setOptionChange(val)
+  }
   console.log(products)
   if(!products){
       return (
@@ -44,6 +58,15 @@ function ProductRecs() {
     <div className='product_wraper'>
         <div className='filter_wrap'>
             <Filter resetFilter={resetFilter} filterByBrands={filterByBrands} filterByCategories={filterByCategories} products={products} />
+            
+            <div className="select_wrap">
+            <div className="filter_by_price">Filter by Price:</div>
+              <select onChange={(e) => sortPrice(e.target.value)}>
+                <option value='default'>default</option>
+                <option value='accending'>Low to High</option>
+                <option value='decending'>High to low</option>
+            </select>
+            </div>
         </div>
         <div className='product_block'>
         {activeProducts.map((products, index) => {
